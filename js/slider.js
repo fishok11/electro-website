efb99f0520f4c55136edac50eeb6075c51482bb6
fishok11 <svllsd811@gmail.com>
2022-04-09 09:55:50 +0300

    SlideAE

--- js/slider.js
+++ js/slider.js
@@ -1,17 +1,20 @@
 let slideIndex = 0;
+const slides = document.getElementsByClassName("main-slider__item");
 
-document.getElementsByClassName("slider-button__left")[0].addEventListener("click", function() {
+document.getElementsByClassName("slider-button__left")[0].addEventListener("click", function(e) {
+    e.preventDefault();
+
     if ( slideIndex > 0 ) {
-        const slides = document.getElementsByClassName("main-slider__item");
         slides[slideIndex].classList.remove('main-slider__item--active');
         slideIndex--;
         slides[slideIndex].classList.add('main-slider__item--active');
     }
 });
 
-document.getElementsByClassName("slider-button__right")[0].addEventListener("click", function() {
-    if ( slideIndex < 2 ) {
-        const slides = document.getElementsByClassName("main-slider__item");
+document.getElementsByClassName("slider-button__right")[0].addEventListener("click", function(e) {
+    e.preventDefault();
+
+    if ( slideIndex < (slides.length - 1) ) {
         slides[slideIndex].classList.remove('main-slider__item--active');
         slideIndex++;
         slides[slideIndex].classList.add('main-slider__item--active');
