ecf49a83992500199abb2a8345440dcf16c3de25
fishok11 <svllsd811@gmail.com>
2022-04-14 15:21:00 +0300

    mst

--- js/product.js
+++ js/product.js
@@ -1,12 +1,15 @@
+const сardWidth = document.querySelector(".main-product__item");
+const сardMargin = parseInt(getComputedStyle(сardWidth, true).marginRight);
+const card = сardWidth.offsetWidth + сardMargin;
+
 document.getElementsByClassName("navline-item__link-left")[0].addEventListener("click", function(e) {
     e.preventDefault();
 
     let cardItems = document.querySelector(".main-product");
     cardItems.scrollBy ({
-        left: -399,
+        left: -(card),
         behavior: "smooth"
     });
-    console.log(0)
 });
 
 
@@ -15,8 +18,7 @@ document.getElementsByClassName("navline-item__link-right")[0].addEventListener(
 
     let cardItems = document.querySelector(".main-product");
     cardItems.scrollBy ({
-        left: 399,
+        left: card,
         behavior: "smooth"
     });
-    console.log(1)
 });
