94905d207c5e3cca4c11afae1edd3076b9775618
fishok11 <svllsd811@gmail.com>
2022-04-17 20:53:48 +0300

    auto swip

--- js/slider.js
+++ js/slider.js
@@ -8,7 +8,8 @@ document.getElementsByClassName("slider-button__left")[0].addEventListener("clic
         slides[slideIndex].classList.remove('main-slider__item--active');
         slideIndex--;
         slides[slideIndex].classList.add('main-slider__item--active');
-    }
+        clearInterval(interval)
+    } 
 });
 
 document.getElementsByClassName("slider-button__right")[0].addEventListener("click", function(e) {
@@ -18,10 +19,21 @@ document.getElementsByClassName("slider-button__right")[0].addEventListener("cli
         slides[slideIndex].classList.remove('main-slider__item--active');
         slideIndex++;
         slides[slideIndex].classList.add('main-slider__item--active');
+        clearInterval(interval)
     } 
 });
 
+function sliderSwipAuto() {
 
+    if ( slideIndex >= (slides.length - 1) ) {
+        slides[slideIndex].classList.remove('main-slider__item--active');
+        slideIndex = 0
+        slides[slideIndex].classList.add('main-slider__item--active');
+    } else {
+        slides[slideIndex].classList.remove('main-slider__item--active');
+        slideIndex++;
+        slides[slideIndex].classList.add('main-slider__item--active');
+    } 
+}
 
-
-
+const interval = setInterval(sliderSwipAuto, 4000)
